4a1da6665040fa51080d3d797e39680773614e9b
DahliaNoir71 <serge.pfeiffer@proton.me>
2024-10-16 09:54:16 +0200

    Add legend to stations map
    Enhanced the map visualization by including a legend. This helps users to better understand the map's data representation and improves overall usability.

--- services/routes_services.py
+++ services/routes_services.py
@@ -13,5 +13,6 @@ def register_routes(app):
         city_stations = jcds.get_stations()
         city_centered_map = maps.get_centered_map(jcds.DEFAULT_CONTRACT_CITY)
         stations_map = maps.add_locations_to_map(city_centered_map, city_stations)
+        stations_map_with_legend = maps.add_legend_to_map(stations_map)
 
-        return stations_map.get_root().render()
+        return stations_map_with_legend.get_root().render()
